1f8c5929cf677207996d88a6551bc61c08dc11c8
noobmaster122 <brahimi.a.farouk@gmail.com>
2023-07-21 22:05:13 +0200

    updated ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   important: true,
   content: ['./src/**/*.{html,ts}'], // Add the content option with the appropriate glob pattern.
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary: '#8B8000'
+      }
+    },
   },
 }
 
